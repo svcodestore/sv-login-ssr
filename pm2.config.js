5e57bc6f8b052bf0a8f4d23d085c98d53d457395
yangwb1123 <1832227346@qq.com>
2022-05-04 07:55:34 +0800

    fix: pm2 config

--- pm2.config.js
+++ pm2.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'nest-react-ssr-app',
+      name: 'sv-login',
       script: 'dist/main.js',
       exec_mode: 'cluster',
       max_memory_restart: '500M',
